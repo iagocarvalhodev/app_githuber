cb8b3012dfadeb4b59afa47398e202a72e9185c1
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-04-27 14:26:15 -0300

    link repo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { withNavigation } from 'react-navigation';
 
 import {
-  View, Text, TouchableOpacity, StatusBar,
+  View, Text, TouchableOpacity, StatusBar, AsyncStorage,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import styles from './styles';
 
-// import { Container } from './styles';
+class Header extends Component {
+  // static PropTypes = {
+  //   title: PropTypes.string.isRequired,
+  // };
 
-const Header = ({ title }) => (
-  <View style={styles.container}>
-    <StatusBar barStyle="dark-content" />
-    <View style={styles.left} />
-    <Text style={styles.title}>{title}</Text>
-    <TouchableOpacity onPress={() => {}} />
-  </View>
-);
+  state = {};
 
-Header.PropTypes = {
-  title: PropTypes.string.isRequired,
-};
+  signOut = async () => {
+    const { navigation } = this.props;
+    await AsyncStorage.clear();
+    navigation.navigate('Welcome');
+  };
 
-export default Header;
+  render() {
+    const { title } = this.props;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" />
+        <View style={styles.left} />
+        <Text style={styles.title}>{title}</Text>
+        <TouchableOpacity onPress={this.signOut}>
+          <Icon name="exchange" size={16} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default withNavigation(Header);
