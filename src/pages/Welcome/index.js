b27c9e0742482c7fbf4f2e188d4189ad6bb78ab4
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-04-27 10:15:09 -0300

    estilizando a pagina welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
 
 // import { Container } from './styles';
+import styles from './style';
 
-const Welcome = () => (
-  <View>
-    <Text>Testando</Text>
-  </View>
-);
+export default class Welcome extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Bem Vindo</Text>
+        <Text style={styles.text}>
+          Para continuar precisamos que você informe seu usuario do github.
+        </Text>
 
-export default Welcome;
+        <View style={styles.form}>
+          <TextInput
+            style={styles.input}
+            autoCapitalize="none"
+            autoCorrect={false}
+            placeholder="Digite seu usuário"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text style={styles.buttonText}>Prosseguir</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
