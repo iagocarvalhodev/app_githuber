6b16c582c88ba4a77f8d8bf8e5cf960625e492a5
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-04-27 16:48:40 -0300

    criando as listas de repositorios

--- src/pages/Organizations/index.js
+++ src/pages/Organizations/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import Header from '~/components/Header';
 
 // import { Container } from './styles';
+// AsyncStorage.clear();
 
-const Organizations = () => <View />;
+const Organizations = () => (
+  <View>
+    <Header title="Organizações" />
+    <Text>Repositories</Text>
+  </View>
+);
+
+const TabIcon = ({ tintColor }) => <Icon name="building" size={20} color={tintColor} />;
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+Organizations.navigationOptions = {
+  tabBarIcon: TabIcon,
+};
 
 export default Organizations;
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,17 +1,65 @@
-import React from 'react';
-
-import { View, Text } from 'react-native';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  View, AsyncStorage, ActivityIndicator, FlatList,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import Header from '~/components/Header';
+import api from '~/services/api';
+import styles from './styles';
 
-// import { Container } from './styles';
+import RepositoryItem from './RepositoryItem';
+
 // AsyncStorage.clear();
+const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
+TabIcon.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+export default class Repositories extends Component {
+  static navigationOptions = {
+    tabBarIcon: TabIcon,
+  };
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositorios" />
-    <Text>Repositories</Text>
-  </View>
-);
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
 
-export default Repositories;
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ refreshing: true });
+    const username = await AsyncStorage.getItem('@Githuber:username');
+
+    const { data } = await api.get(`/users/${username}/repos`);
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadRepositories}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <View style={styles.container}>
+        <Header title="Repositorios" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
